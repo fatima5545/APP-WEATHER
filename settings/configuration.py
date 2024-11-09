bcfec1b6d1037f77abf7d7acea82927c42f971d5
fatimanid <fatima.nidbahdou.ext@symphonics.fr>
2024-11-09 23:12:21 +0100

    Reformate config gile

--- settings/configuration.py
+++ settings/configuration.py
@@ -3,6 +3,7 @@ import os
 
 from settings.logging_config import logger
 
+
 class Settings(BaseSettings):
     WEATHERBIT_API_KEY: str = os.getenv(
         "WEATHERBIT_API_KEY"
@@ -12,7 +13,7 @@ class Settings(BaseSettings):
     )  # Valeur par défaut pour Redis en local
     REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))
     REDIS_DB: int = int(os.getenv("REDIS_DB", 0))
-    WEATHERBIT_API_BASE_URL: str = "https://api.weatherbit.io/v2.0/" 
+    WEATHERBIT_API_BASE_URL: str = "https://api.weatherbit.io/v2.0/"
 
     class Config:
         env_file = ".env"  # Utilise le fichier .env si les variables d'environnement ne sont pas définies
@@ -35,6 +36,7 @@ class Settings(BaseSettings):
                 f"Les variables d'environnement suivantes sont manquantes : {', '.join(missing_vars)}. "
                 "Veuillez vérifier votre fichier .env ou vos variables d'environnement système."
             )
+
 
 # Initialiser les paramètres et vérifier les variables manquantes
 settings = Settings()
