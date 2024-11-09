5ef3b5aee8e36a25837e0d1e28f2da533c82ad5f
fatimanid <fatima.nidbahdou.ext@symphonics.fr>
2024-11-09 22:59:35 +0100

    Add checks for missing environment variables

--- settings/configuration.py
+++ settings/configuration.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings
 import os
 
+from settings.logging_config import logger
 
 class Settings(BaseSettings):
     WEATHERBIT_API_KEY: str = os.getenv(
@@ -15,5 +16,25 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"  # Utilise le fichier .env si les variables d'environnement ne sont pas définies
 
+    def check_missing_variables(self):
+        """Vérifie si des variables d'environnement sont manquantes et les loggue."""
+        missing_vars = []
 
+        if not self.WEATHERBIT_API_KEY:
+            missing_vars.append("WEATHERBIT_API_KEY")
+        if not self.REDIS_HOST:
+            missing_vars.append("REDIS_HOST")
+        if not self.REDIS_PORT:
+            missing_vars.append("REDIS_PORT")
+        if not self.REDIS_DB:
+            missing_vars.append("REDIS_DB")
+
+        if missing_vars:
+            logger.error(
+                f"Les variables d'environnement suivantes sont manquantes : {', '.join(missing_vars)}. "
+                "Veuillez vérifier votre fichier .env ou vos variables d'environnement système."
+            )
+
+# Initialiser les paramètres et vérifier les variables manquantes
 settings = Settings()
+settings.check_missing_variables()
